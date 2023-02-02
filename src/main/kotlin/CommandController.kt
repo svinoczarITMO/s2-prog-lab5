5c648916826c7d28762abde5064b5d5f333f9825
notsunshine <alexsupper75@gmail.com>
2023-02-02 22:08:49 +0300

    [ADD] all needed commands

--- src/main/kotlin/CommandController.kt
+++ src/main/kotlin/CommandController.kt
@@ -1,19 +1,29 @@
+import kotlin.system.exitProcess
+
 class CommandController {
     companion object {
-        fun help() {}
-        fun info() {}
-        fun show() {}
-        fun insert(key: String) {
+        fun help () {}
+        fun info () {}
+        fun show () {}
+        fun insert (key: String) {
             print("Введите значение элемента: ")
             var element = readln()
         }
-        fun update(id: Int) {
+        fun update (id: Int) {
             print("Введите значение элемента: ")
             var element = readln()
         }
         fun removeKey(element: String) {}
-        fun clear() {}
-        fun save() {}
+        fun clear () {}
+        fun save () {}
         fun executeScript(fileName: String) {}
+        fun exit () {
+            exitProcess(0)
+        }
+        fun removeGreater (element: String) {}
+        fun countGreaterThanDescription (description: String) {}
+        fun filterLessThanGenre (genre: MusicGenre) {}
+        fun printFieldDescendingNumberOfParticipants () {}
+
     }
 }
