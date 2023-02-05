d8fa5ca3d3181f15015872b04d01f1e305e0a862
notsunshine <alexsupper75@gmail.com>
2023-02-06 02:04:28 +0300

    [MODIFY] add dev fun getElement

--- src/main/kotlin/CollectionController.kt
+++ src/main/kotlin/CollectionController.kt
@@ -1,6 +1,25 @@
 import java.util.Vector
 
 class CollectionController {
-    private var Person = Vector<Person>()
 
+    companion object {
+        var vector = Vector<Person>()
+
+        fun addElement(element: Person) {
+            vector.add(element)
+        }
+        fun getElement (id: Int) { //TODO сделать через рефлексию (?)
+            val obj = vector[id]
+            println("Объект ${obj.id}:\n"
+            + "Дата создания: \"" + obj.creationDate + "\" \n"
+            + "Координаты: x = " + obj.coordinates.x + " y = " + obj.coordinates.y + "\n"
+            + "Имя: " + obj.name + "\n"
+            + "Рост: " + obj.height + "\n"
+            + "Вес: " + obj.weight + "\n"
+            + "Цвет волос: " + obj.hairColor + "\n"
+            + "Национальность: " + obj.nationality
+            + "Местоположение: x = " + obj.location.x + "; y = " + obj.location.y + "; z = " + obj.location.z + "\n")
+
+        }
+    }
 }
