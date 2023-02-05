bc43737c43e5b62c4a6bc8d55ade8ab4643c8361
notsunshine <alexsupper75@gmail.com>
2023-02-05 13:47:25 +0300

    [MODIFY] setup addEl fun

--- src/main/kotlin/CommandController.kt
+++ src/main/kotlin/CommandController.kt
@@ -1,7 +1,37 @@
+import java.time.ZonedDateTime
+import kotlin.random.Random
 import kotlin.system.exitProcess
 
 class CommandController {
     companion object {
+        fun addEl () {
+            var id: Int = 1
+
+            print("Введите имя: ");
+            var name: String = readln()
+            print("Введите координату x: ");
+            var coordinateX: Float = readln().toFloat()
+            print("Введите координату y: ");
+            var coordinateY: Double = readln().toDouble()
+            var coordinates: Coordinates = Coordinates(coordinateX, coordinateY)
+
+            var creationDate: java.time.ZonedDateTime = ZonedDateTime.now()
+
+            print("Введите numberOfParticipants: ");
+            var numberOfParticipants: Int = readln().toInt()
+            print("Введите albumsCount: ");
+            var albumsCount: Long = readln().toLong()
+            print("Введите description: ");
+            var description: String = readln()
+            print("Введите genre: ");
+            var genre: String = readln()
+            print("Введите bestAlbum: ");
+            var bestAlbum: String = readln()
+
+            var musicElement: MusicBand = MusicBand(id, name, coordinates, creationDate, numberOfParticipants, )
+
+            id++
+        }
         fun help () {}
         fun info () {}
         fun show () {}
@@ -30,4 +60,13 @@ class CommandController {
         fun printFieldDescendingNumberOfParticipants () {}
 
     }
+//    id: Int,
+//    name: String,
+//    coordinates: Coordinates,
+//    creationDate: ZonedDateTime,
+//    numberOfParticipants: Int,
+//    albumsCount: Int,
+//    description: String,
+//    genre: MusicGenre,
+//    bestAlbum: Album
 }
