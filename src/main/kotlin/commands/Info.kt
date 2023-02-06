b003760ee015c0f47363d55c7947e6127f0e5c92
notsunshine <alexsupper75@gmail.com>
2023-02-06 23:49:45 +0300

    [MODIFY] "freeze" Info.kt, commented all

---
+++ src/main/kotlin/commands/Info.kt
@@ -0,0 +1,29 @@
+package commands
+
+import CollectionController
+import kotlin.reflect.full.instanceParameter
+import kotlin.reflect.full.valueParameters
+import kotlin.reflect.jvm.reflect
+
+class Info {
+    companion object{
+//    private var reflexVector = CollectionController::vector
+//    private var type = reflexVector.returnType
+//    private var size = reflexVector.valueParameters.size
+//    private var accessability = reflexVector.isOpen
+//    private var annotations = reflexVector.annotations
+//    private var param = reflexVector.parameters
+//    private var typeParam = reflexVector.typeParameters
+//    private var paramm = reflexVector.instanceParameter
+//        fun printInfo () {
+//            println(type.toString() + ", " +
+//                    size.toString() + ", " +
+//                    accessability.toString() + ", " +
+//                    annotations.toString() + ", " +
+//                    param.toString() + ", " +
+//                    typeParam.toString() + ", " +
+//                    param.toString() + " " )
+//        }
+    }
+
+}
