f4b822da294215cd1ada37d593e7a74edbbb27f0
notsunshine <alexsupper75@gmail.com>
2023-02-07 00:30:26 +0300

    [MODIFY] remove useless strings in fun printInfo

--- src/main/kotlin/CollectionController.kt
+++ src/main/kotlin/CollectionController.kt
@@ -1,23 +1,19 @@
-import commands.Info
 import java.util.Vector
-import kotlin.reflect.full.instanceParameter
-import kotlin.reflect.full.valueParameters
+
 
 class CollectionController {
-
     companion object {
         var vector = Vector<Person>()
+
         private var reflexVector = ::vector
         private var type = reflexVector.returnType
         private var accessability = reflexVector.isOpen
-        private var annotations = reflexVector.annotations
-        private var param = reflexVector.parameters
-        private var typeParam = reflexVector.typeParameters
-        private var paramm = reflexVector.instanceParameter
+
 
         fun addElement(element: Person) {
             vector.add(element)
         }
+
         fun getElement (id: Int) { //TODO сделать через рефлексию (?)
             val obj = vector[id]
             println("Объект ${obj.id}:\n"
@@ -27,21 +23,17 @@ class CollectionController {
             + "Рост: " + obj.height + "\n"
             + "Вес: " + obj.weight + "\n"
             + "Цвет волос: " + obj.hairColor + "\n"
-            + "Национальность: " + obj.nationality
+            + "Национальность: " + obj.nationality + "\n"
             + "Местоположение: x = " + obj.location.x + "; y = " + obj.location.y + "; z = " + obj.location.z + "\n")
-
         }
+
         fun printInfo () {
             println(
                type.toString().split(".")[2] + ", "
                + "Размер: " + vector.size + ", "
                + when { accessability.toString() == "false, " -> "close"
-                   else -> "open, " }
-               + annotations.toString() + ", "
-               + param.toString() + ", "
-               + typeParam.toString() + ", "
-               + param.toString() + ", "
-               + paramm.toString())
+                   else -> "open, " } )
         }
     }
+
 }
