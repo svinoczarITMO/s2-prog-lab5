a933fa1e1496f59e5128c2e2278841d26d1495ed
notsunshine <alexsupper75@gmail.com>
2023-02-06 23:50:32 +0300

    [MODIFY] edit command "info" in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,36 @@
 import commands.Help
 import commands.History
+import commands.Info
 
-class Main {
-    fun main(args: Array<String>) {
+fun main() {
 
-        while (true){
-            val command: List<String> = readln().lowercase().split(" ")
-            History.writeInBuffer(command[0])
-            when (command[0]) {
-                "/print" -> CommandController.prnt()
-                "/get" -> CommandController.get((command[1]).toInt())
-                "/fadd" -> CommandController.fadd()
+    while (true){
+        val command: List<String> = readln().lowercase().split(" ")
+        History.writeInBuffer(command[0])
+        when (command[0]) {
+            "/print" -> CommandController.prnt()
+            "/get" -> CommandController.get((command[1]).toInt())
+            "/fadd" -> CommandController.fadd()
 
-                "help" -> Help.help()
-                "info" -> CommandController.info()
-                "show" -> CommandController.show()
-                "add" -> CommandController.add()
-                "update" -> CommandController.update((command[1]).toInt())
-                "remove_by_id" -> CommandController.removeById((command[1]).toInt())
-                "clear" -> CommandController.clear()
-                "save" -> CommandController.save()
-                "execute_script" -> CommandController.executeScript(command[1])
-                "exit" -> CommandController.exit()
-                "remove_first" -> CommandController.removeFirst()
-                "reorder" -> CommandController.reorder()
-                "history" -> History.printHistory()
-                "min_by_weight" -> CommandController.minByWeight()
-                "group_counting_by_nationality" -> CommandController.groupCountingByNationality()
-                "count_by_hair_color" -> CommandController.countByHairColor()
+            "help" -> Help.help()
+            "info" -> CollectionController.printInfo()
+            "show" -> CommandController.show()
+            "add" -> CommandController.add()
+            "update" -> CommandController.update((command[1]).toInt())
+            "remove_by_id" -> CommandController.removeById((command[1]).toInt())
+            "clear" -> CommandController.clear()
+            "save" -> CommandController.save()
+            "execute_script" -> CommandController.executeScript(command[1])
+            "exit" -> CommandController.exit()
+            "remove_first" -> CommandController.removeFirst()
+            "reorder" -> CommandController.reorder()
+            "history" -> History.printHistory()
+            "min_by_weight" -> CommandController.minByWeight()
+            "group_counting_by_nationality" -> CommandController.groupCountingByNationality()
+            "count_by_hair_color" -> CommandController.countByHairColor()
 
-                else -> println("Такой команды не существует.")
+            else -> println("Такой команды не существует.")
 
-            }
         }
     }
 }
