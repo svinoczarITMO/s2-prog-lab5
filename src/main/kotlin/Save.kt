4b513b9238405d6f0e54233911c9dc64765bbc9f
notsunshine <alexsupper75@gmail.com>
2023-02-08 16:33:54 +0300

    [ADD] create readFromFile function

---
+++ src/main/kotlin/Save.kt
@@ -0,0 +1,15 @@
+import CollectionController
+import java.io.File
+import java.io.FileReader
+import java.io.InputStreamReader
+
+class Save {
+    companion object {
+        val pathToFile = "D:\\ITMO\\2nd-semester\\prog-labs\\s2-prog-lab5\\src\\main\\kotlin\\Collection.xml"
+        fun readFromFile () {
+            var readerBuffer = File(pathToFile).readText()
+            //TODO: regex
+            var tags =
+        }
+    }
+}
