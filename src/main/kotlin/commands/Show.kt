f43fc34b8182cf921e04efb19b7e730eea535001
notsunshine <alexsupper75@gmail.com>
2023-02-10 01:53:54 +0300

    [MODIFY] upgrade "show" command

--- src/main/kotlin/commands/Show.kt
+++ src/main/kotlin/commands/Show.kt
@@ -5,8 +5,15 @@ import CollectionController
 class Show {
     companion object {
         fun printAllElementsAsString() {
-            for (i in CollectionController.vector) {
-                print("$i, ")
+            val vector = CollectionController.vector
+            if (vector.size > 1) {
+                for (i in 0..CollectionController.vector.size - 1) {
+                    print("${vector[i].name}, ")
+                }
+            } else if (vector.size == 1) {
+                print(vector.lastElement().name)
+            } else {
+                println("В коллекции не содержится элементов. ")
             }
         }
     }
